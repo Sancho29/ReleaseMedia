da5d78c1da74d33731143bd259de91bb64356127
Sancho29 <chris_martins@hotmail.com>
2012-08-07 21:32:32 +0100

    Full site upload

--- mailer.php
+++ mailer.php
@@ -1,9 +1,7 @@
-<!DOCTYPE html>
 <head>
-<meta name="google-site-verification" content="QgFlQie-5tXZywIBAqQd9X7lyORWd2Hdzt4MPuSWCbo" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" href="css/styles.css" type="text/css" />
-<title>ReleaseMedia - a multimedia consultancy based in Hampshire</title>
+<title>ReleaseMedia - Thanks for contacting us!</title>
 <meta name="description" content="ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications, based in Hampshire. Graphic design, Web design, animation, 3D modelling, e-Learning, photography, audio editing, video editing. ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications, based in Hampshire. Graphic design, Web design, animation, 3D modelling, e-Learning, photography, audio editing, video editing. ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications, based in Hampshire. Graphic design, Web design, animation, 3D modelling, e-Learning, photography, audio editing, video editing. ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications, based in Hampshire. Graphic design, Web design, animation, 3D modelling, e-Learning, photography, audio editing, video editing. ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications, based in Hampshire. Graphic design, Web design, animation, 3D modelling, e-Learning, photography, audio editing, video editing. ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications, based in Hampshire. Graphic design, Web design, animation, 3D modelling, e-Learning, photography, audio editing, video editing. ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications, based in Hampshire. Graphic design, Web design, animation, 3D modelling, e-Learning, photography, audio editing, video editing. "/>
 </head>
 
@@ -26,16 +24,16 @@ var currentTime = new Date()
 var month = currentTime.getMonth() + 1
 var day = currentTime.getDate()
 var year = currentTime.getFullYear()
-document.write(day + "." + month +  "." + year)
+document.write(day + "." + month + "." + year)
 //-->
 </script>
 </p>
     </div>
     <div id="menu">
     <ul id="nav">
-      <li><a href="#" class="navon">Home</a></li>
+      <li><a href="index.html">Home</a></li>
       <li><a href="work.html">Porfolio</a></li>
-      <li><a href="contact.html">Contact</a></li>
+      <li><a href="#" class="navon">Contact</a></li>
     </ul>
   </div>
 
@@ -46,20 +44,37 @@ document.write(day + "." + month +  "." + year)
     <div id="top">
     <div class="topleft">
     <div class="title">
-    	<p>Services</p>
+    	<p>Talk to us</p>
     </div>
     <div class="text">
-    <p>ReleaseMedia is a multimedia consultancy providing services in all areas of multimedia, from small business web design/development to large commercial consultancy contracts on bespoke training applications.</p><br />
-    <p>A professional and skilled consultancy with many years experience in all areas of multimedia. We are dedicated to all projects we undertake and take great satisfaction in exceeding our clients expectations.</p><br />
-    <p>Based in central Hampshire and working throughout the county, we are also willing to travel for the bespoke needs of our costumers.</p>
+    <p>We are dedicated to all projects we undertake, we take great satisfaction in exceeding our clients expectations.</p>
+    <p>We protect the privatacy of our customers and enquirers, please leave a query below and we will be in contact very soon with the answers.</p>
     </div>
     </div> <!--topleft-->
-    <div class="image"><img src="images/box1.jpg" alt="Project management, web design, web development, sound editing, illustration, graphic design, e-learning, photography, 3D modelling, flash, video editing"></div>
+    <div class="image">
+    
+    </div>
     </div> <!--top-->
     
     <div class="services">
-    <img src="images/services.jpg" alt="Project management, web design, web development, sound editing, illustration, graphic design, e-learning, photography, 3D modelling, flash, video editing">
-    </div>
+        <?php
+		if(isset($_POST['submit'])) {
+		$to = "chris@cmd-media.co.uk";
+		$subject = "ReleaseMedia Ltd Enquiry";
+		$name_field = $_POST['name'];
+		$telephone_field = $_POST['telephone'];
+		$email_field = $_POST['email'];
+		$message = $_POST['message'];
+		 
+		$body = "From: $name_field\n Telephone: $telephone_field\n E-Mail: $email_field\n Message:\n $message";
+		 
+		echo "<br>Thanks for taking the time to fill out an enquiry, it has been submitted to $to.<br><br>We will get back to you as soon as possible, with an answer to your enquiry.";
+		mail($to, $subject, $body);
+		} else {
+		echo "Thanks";
+		}
+		?>
+      </div>
 
 </div> <!--content-->
 
@@ -70,7 +85,7 @@ document.write(day + "." + month +  "." + year)
     </div>
     
     <div class="links">
-    &gt; <a class="link" title="CMD Portfolio" href="http://www.cmd-media.co.uk/">CMD Media</a>
+    &gt; <a class="link" title="CMD Portfolio" href="http://www.cmd-media.co.uk/">Chris' Portfolio</a>
 	<br>
 	&gt; <a class="link" title="Smashing Magazine" href="http://www.smashingmagazine.co.uk">Smashing Magazine</a>
 	<br>
