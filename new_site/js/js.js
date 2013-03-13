a1afd9944ef7eafbbb570a69f9bd21be5256645d
Sancho29 <chris_martins@hotmail.com>
2013-03-13 22:28:43 +0000

    Bug fixes

--- new_site/js/js.js
+++ new_site/js/js.js
@@ -1,20 +1,5 @@
 
 $(document).ready(function(){
-    // defaults
-    // var st, win = $(window)[0],
-    //     body = $('body')[0],
-    //     doc = (jQuery.support.boxModel) ? document.documentElement : document.body,
-    //     wrap = $('#wrapper')[0],
-
-    // // Set top background image height here
-    //     imgH = 3000; // top image height
-
-    // // vertical parallax scroll
-    // $(win).scroll(function(){
-    //     st = (win.pageYOffset || doc.scrollTop );
-    //     if (st < imgH) { wrap.style.backgroundPosition = 'center ' + (st/4) + 'px'; } // limit moving top image only when in view
-    //     body.style.backgroundPosition = 'left ' + ( imgH + st/4) + 'px';
-    // });
 
     function scrollToAnchor(id){
         var goTo = $(id);
@@ -31,17 +16,16 @@ $(document).ready(function(){
 
     $('.link.mobile').click(function(e) {
         e.preventDefault();
-        $('nav.nav-mobile').toggleClass('show');
-        $('header').toggleClass('expand');
-        $('.block.home').toggleClass('expand');
+        $("nav.nav-mobile").slideUp("slow");  
     })
 
     $('.btn-navbar').click(function(e) {
         e.preventDefault();
-                              
-        $('nav.nav-mobile').toggleClass('show');
-        $('header').toggleClass('expand');
-        $('.block.home').toggleClass('expand');
+        if ($('nav.nav-mobile').is(':hidden')) {
+            $("nav.nav-mobile").slideDown("slow"); 
+        } else {
+            $("nav.nav-mobile").slideUp("slow");  
+        }
     });
 
 });
